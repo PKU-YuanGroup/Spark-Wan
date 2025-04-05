747dd0a674ddb8a589f6bf0478794158a64c3618
SHYuanBest <shyuan-cs@hotmail.com>
2025-04-05 20:41:53 +0800

    update

--- scripts/infer_t2v.sh
+++ scripts/infer_t2v.sh
@@ -1,10 +1,11 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 torchrun --nproc_per_node=8 infer.py \
-    --model_path "/mnt/workspace/checkpoints/Wan-AI/Wan2.1-I2V-14B-720P-Diffusers/" \
+    --model_path "/mnt/workspace/checkpoints/Wan-AI/Wan2.1-T2V-14B-Diffusers/" \
     --sp_size 8 \
     --height 720 \
     --width 1280 \
     --num_frames 81 \
-    --sampling_steps 8 \
+    --sampling_steps 32 \
     --seed 2002 \
-    --prompt_file scripts/prompt_i2v.json
+    --prompt_file scripts/prompt_t2v.txt \
+    --flow_shift 7.0
